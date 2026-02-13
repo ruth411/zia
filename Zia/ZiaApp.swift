faa8d4fd79b3fec131422434ccf7c1e5478be4eb
ruth411 <ruthwikdovala@gmail.com>
2026-02-13 13:02:10 -0500

    Initial Commit

---
+++ Zia/ZiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZiaApp.swift
+//  Zia
+//
+//  Created by Ruthwik Dovala on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ZiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
